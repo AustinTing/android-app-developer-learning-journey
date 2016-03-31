5ee0e9ddd9056337abfa99579f641a33668571b5
CellBody <austintingwork@gmail.com>
2016-03-31 18:34:02 +0800

    Update Android 6 Tutorial
    Learn and practice from 張益裕's Android Tutorial 第二堂（2）設計應用程式使用者介面
    http://www.codedata.com.tw/mobile/android-tutorial-the-2nd-class-2-ui/

--- practices/Android 6 Tutorial/app/src/main/java/com/gmail/austintingwork/android6tutorial/MainActivity.java
+++ practices/Android 6 Tutorial/app/src/main/java/com/gmail/austintingwork/android6tutorial/MainActivity.java
@@ -3,6 +3,8 @@ package com.gmail.austintingwork.android6tutorial;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        int layoutId = android.R.layout.simple_list_item_1;
+        String[] data = {
+            "Android 6 Turtorial",
+            "Learn and Practice",
+            "Have Fun"
+        };
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,layoutId, data);
+        ListView listView = (ListView)findViewById(R.id.item_list);
+        listView.setAdapter(adapter);
     }
 
     public boolean onCreateOptionsMenu(Menu menu){
