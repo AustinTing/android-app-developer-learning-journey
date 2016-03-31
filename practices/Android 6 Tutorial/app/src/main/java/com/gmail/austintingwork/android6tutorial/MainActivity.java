2c54ecb33ed057b791cbf1aa5f17c981f4d27154
CellBody <austintingwork@gmail.com>
2016-03-31 12:03:48 +0800

    Update Android 6 Tutorial
    Learn and practice from 張益裕's Android Tutorial 第二堂（1）規劃與建立應用程式需要的資源
    http://www.codedata.com.tw/mobile/android-tutorial-the-2nd-class-1-res/

--- practices/Android 6 Tutorial/app/src/main/java/com/gmail/austintingwork/android6tutorial/MainActivity.java
+++ practices/Android 6 Tutorial/app/src/main/java/com/gmail/austintingwork/android6tutorial/MainActivity.java
@@ -2,6 +2,7 @@ package com.gmail.austintingwork.android6tutorial;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 }
