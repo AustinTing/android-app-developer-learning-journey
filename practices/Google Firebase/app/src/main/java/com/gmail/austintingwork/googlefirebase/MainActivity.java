4ab8a2d1ca9c8192441445c5f1de4ee389c6a788
CellBody <austintingwork@gmail.com>
2016-07-05 23:08:47 +0800

    Update Google Firebase
    Practice from http://litotom.com/2016/05/20/2016-firebase/

--- practices/Google Firebase/app/src/main/java/com/gmail/austintingwork/googlefirebase/MainActivity.java
+++ practices/Google Firebase/app/src/main/java/com/gmail/austintingwork/googlefirebase/MainActivity.java
@@ -1,7 +1,16 @@
 package com.gmail.austintingwork.googlefirebase;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +18,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        GET KEY HASH
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo("com.gmail.austintingwork.googlefirebase", PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md;
+                md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                //String something = new String(Base64.encodeBytes(md.digest()));
+                String KeyResult = new String(Base64.encode(md.digest(), 0));
+                Log.d("KeyHash&ID", "KeyHash :" + KeyResult);
+
+            }
+        } catch (PackageManager.NameNotFoundException e1) {
+            Log.e("name not found", e1.toString());
+
+        } catch (NoSuchAlgorithmException e) {
+            Log.e("no such an algorithm", e.toString());
+        } catch (Exception e) {
+            Log.e("exception", e.toString());
+        }
+
     }
 }
--- practices/Google Firebase/app/src/main/java/com/gmail/austintingwork/googlefirebase/MyInstanceIDService.java
+++ practices/Google Firebase/app/src/main/java/com/gmail/austintingwork/googlefirebase/MyInstanceIDService.java
@@ -1,8 +1,18 @@
 package com.gmail.austintingwork.googlefirebase;
 
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
 /**
  * Created by cellbody on 2016/7/4.
  */
 
-public class MyInstanceIDService {
+public class MyInstanceIDService extends FirebaseInstanceIdService {
+    @Override
+    public void onTokenRefresh() {
+        String token = FirebaseInstanceId.getInstance().getToken();
+        Log.d("FCM", "Token:"+token);
+    }
 }
