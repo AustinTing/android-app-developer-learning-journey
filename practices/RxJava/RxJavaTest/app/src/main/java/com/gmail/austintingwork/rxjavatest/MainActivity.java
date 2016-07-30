6057394644459fce2ab459607a4239a9ea46c17a
CellBody <austintingwork@gmail.com>
2016-07-30 15:46:35 +0800

    Update RxJava
    Learn form Grokking RxJava, Part 2: Operator, Operator
    http://blog.danlew.net/2014/09/22/grokking-rxjava-part-2/

--- practices/RxJava/RxJavaTest/app/src/main/java/com/gmail/austintingwork/rxjavatest/MainActivity.java
+++ practices/RxJava/RxJavaTest/app/src/main/java/com/gmail/austintingwork/rxjavatest/MainActivity.java
@@ -5,17 +5,29 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import rx.Observable;
 import rx.Subscriber;
 import rx.functions.Action1;
 import rx.functions.Func1;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "debug";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        examplePart01();
+        examplePart02();
+
+
+    }
+
+
+     private void examplePart01() {
 
 //        example 1-01
         Observable<String> myObservable1 = Observable.create(
@@ -30,26 +42,30 @@ public class MainActivity extends AppCompatActivity {
         Subscriber<String> mySubscriber1 = new Subscriber<String>() {
             @Override
             public void onNext(String s) {
-                Log.d("debug", "Subscriber: onNext: "+s);
+                Log.d("debug", "Subscriber: onNext: " + s);
             }
 
             @Override
-            public void onCompleted() { }
+            public void onCompleted() {
+            }
 
             @Override
-            public void onError(Throwable e) { }
+            public void onError(Throwable e) {
+            }
         };
         myObservable1.subscribe(mySubscriber1);
+
 //        example 1-02 原始
         Observable<String> myObservable2 =
                 Observable.just("Hello, world!");
         Action1<String> onNextAction2 = new Action1<String>() {
             @Override
             public void call(String s) {
-                Log.d("debug", "onNextAction2: onNext: "+s);
+                Log.d("debug", "onNextAction2: onNext: " + s);
             }
         };
-        myObservable2.subscribe(onNextAction2);
+//         obervable結束後執行一個方法？
+//        myObservable2.subscribe(onNextAction2);
 
 
 //        exmaple 1-02 簡化
@@ -68,14 +84,88 @@ public class MainActivity extends AppCompatActivity {
 //        example 1-03 簡化
         Observable.just("Hello, world!")
                 .map(s -> s + " -Dan")
-                .subscribe(s -> System.out.println(s));
+                .subscribe(System.out::println);
 //        example 1-04
         Observable.just("Hello, world!")
-                .map(s -> s +" -Austin")
+                .map(s -> s + " -Austin")
                 .map(s -> s.hashCode())
                 .map(i -> Integer.toString(i))
                 .subscribe(s -> System.out.println(s));
 
+    }
 
+    private void examplePart02() {
+
+//        example 01 from()
+//        query("Hello, world!")
+//                .subscribe(urls -> {
+//                    Observable.from(urls)
+//                            .subscribe(url -> logIt(url));
+//                });
+
+//        example 02 flatMap()
+        query("Hello, world!")
+                .flatMap(new Func1<List<String>, Observable<String>>() {
+                    @Override
+                    public Observable<String> call(List<String> urls) {
+                        return Observable.from(urls);
+                    }
+                })
+                .subscribe(url -> logIt("ex 03: "+url));
+
+//        example 03 flatMap() 簡化
+        query("Hello, world")
+                .flatMap(urls -> Observable.from(urls))
+                .subscribe(url -> logIt("ex 03 簡化: "+url));
+
+//        example 04 flatMap() + flatMap()
+        query("Hellom world")
+                .flatMap(urls -> Observable.from(urls))
+                .flatMap(new Func1<String, Observable<?>>() {
+                    @Override
+                    public Observable<String> call(String s) {
+                        return getTitle(s);
+                    }
+                })
+                .subscribe(title -> logIt("ex 04: " + title));
+//        example 04 flatMap() + flatMap() 簡化
+        query("Hellom world")
+                .flatMap(urls -> Observable.from(urls))
+                .flatMap(url -> getTitle(url))
+                .subscribe(title -> logIt("ex 04 簡化: " + title));
+
+//        example 05 filter()
+        query("Hellom world")
+                .flatMap(urls -> Observable.from(urls))
+                .flatMap(url -> getTitle(url))
+                .filter(title -> title != null)
+                .take(1)
+                .subscribe(title -> logIt("ex 05 " + title));
+
+    }
+
+    Observable<List<String>> query(String text) {
+        List<String> urls = new ArrayList<>();
+        urls.add("url1");
+        urls.add("url2");
+        urls.add("url3");
+
+        Observable<List<String>> myObservable = Observable.just(urls);
+        return myObservable;
+    }
+
+    Observable<String> getTitle(String url){
+        String tittle= url;
+        int rd = Math.random() > 0.5 ? 0 : 1;
+        if (rd  == 1 ) {
+            tittle = null;
+        }
+        Observable<String> observable = Observable.just(tittle);
+        return observable;
+
+    }
+
+    private void logIt(String s) {
+        Log.d(TAG, s);
     }
 }
